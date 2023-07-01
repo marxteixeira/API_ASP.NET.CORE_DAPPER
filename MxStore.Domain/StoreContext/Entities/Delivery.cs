6a9524bae0c22b5366e99af53d43bf098c2b0f36
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-01 18:20:22 -0300

    Modelando o pedido.

--- MxStore.Domain/StoreContext/Entities/Delivery.cs
+++ MxStore.Domain/StoreContext/Entities/Delivery.cs
@@ -7,8 +7,28 @@ namespace MxStore.Domain.StoreContext.Entities
 {
     public class Delivery
     {
+        public Delivery(DateTime estimatedDeliveryDate)
+        {
+            CreatedDate = DateTime.Now;
+            EstimatedDeliveryDate = estimatedDeliveryDate;
+            Status = EDeliveryStatus.Waiting;
+        }
+
         public DateTime CreatedDate { get; set; }
         public DateTime EstimatedDeliveryDate { get; set; }
         public EDeliveryStatus Status { get; set; }
+
+        public void Ship()
+        {
+            //se a data estimada de entrega for no passado, não entregar.
+            Status = EDeliveryStatus.Shipped;
+        }
+
+        //cancelar um pedido
+        public void Cancel()
+        {
+            // se o status já estiver entregue, não pode cancelar.
+            Status = EDeliveryStatus.Canceled;
+        }
     }
 }
