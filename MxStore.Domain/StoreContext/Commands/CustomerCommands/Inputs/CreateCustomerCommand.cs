0fb1ef622ba46f852ee23d2a240b76f5af123fc2
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-03 20:34:04 -0300

    Customer Commands

---
+++ MxStore.Domain/StoreContext/Commands/CustomerCommands/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MxStore.Domain.StoreContext.Commands.CustomerCommands.Inputs
+{
+    public class CreateCustomerCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
