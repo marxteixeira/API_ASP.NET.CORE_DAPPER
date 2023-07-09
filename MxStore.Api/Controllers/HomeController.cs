1fd01f87e21df345e427b4cdb4ac191227707934
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-09 09:02:19 -0300

    adicionando pacote ASP.NET para configurar a Controller

---
+++ MxStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MxStore.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public object asdfasdfGet()
+        {
+            return new {version = "Version 0.0.1"};
+        }
+    }
+}
