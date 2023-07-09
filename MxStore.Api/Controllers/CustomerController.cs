6c33895f96cd92f8cff36cb33fd5c204c872710a
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-09 19:15:53 -0300

    testando endpoits com dados mockados.

--- MxStore.Api/Controllers/CustomerController.cs
+++ MxStore.Api/Controllers/CustomerController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MxStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
 using MxStore.Domain.StoreContext.Entities;
+using MxStore.Domain.StoreContext.ValueObjects;
 using System;
 using System.Collections.Generic;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MxStore.Api.Controllers
 {
@@ -11,42 +14,85 @@ namespace MxStore.Api.Controllers
         [Route("customers")]
         public List<Customer> Get()
         {
-            return null;
+            var name2 = new Name("Teste", "Teixeira");
+            var document2 = new Document("109021746845");
+            var email2 = new Email("email@email.com");
+            var customer2 = new Customer(name2, document2, email2, "32984848484");
+
+            var name = new Name("Marx", "Teixeira");
+            var document = new Document("109021746845");
+            var email = new Email("email@email.com");
+            var customer = new Customer(name, document, email, "32984848484");
+            var customers = new List<Customer>();
+            customers.Add(customer);
+            customers.Add(customer2);
+
+            return customers;
         }
 
         [HttpGet]
         [Route("customers/{id}")]
-        public List<Customer> GetById(Guid id)
+        public Customer GetById(Guid id)
         {
-            return null;
+            var name2 = new Name("Teste", "Teixeira");
+            var document2 = new Document("109021746845");
+            var email2 = new Email("email@email.com");
+            var customer2 = new Customer(name2, document2, email2, "32984848484");
+
+            return customer2;
         }
 
         [HttpGet]
         [Route("customers/{id}/orders")]
         public List<Order> GetOrders(Guid id)
         {
-            return null;
+            var name = new Name("Marx", "Teixeira");
+            var document = new Document("109021746845");
+            var email = new Email("email@email.com");
+            var customer = new Customer(name, document, email, "32984848484");
+
+            var order = new Order(customer);
+            var mouse = new Product("Mouse Gamer", "Mouse Gamer", "mouse.jpg", 100M, 10);
+            var monitor = new Product("monitor Gamer", "monitor Gamer", "momonitoruse.jpg", 100M, 10);
+            order.AddItem(monitor, 5);
+            order.AddItem(mouse, 5);
+
+            var orders = new List<Order>();
+            orders.Add(order);
+
+
+            return orders;
         }
 
         [HttpPost]
         [Route("customers")]
-        public Customer Post([FromBody]Customer customer)
+        public Customer Post([FromBody]CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+
+            return customer;
         }
 
         [HttpPut]
         [Route("customers/{id}")]
-        public Customer Put([FromBody] Customer customer)
+        public Customer Put([FromBody] CreateCustomerCommand command)
         {
-            return null;
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            var customer = new Customer(name, document, email, command.Phone);
+
+            return customer;
         }
 
         [HttpDelete]
         [Route("customers/{id}")]
-        public string Delete()
+        public object Delete()
         {
-            return null;
+            return new {message = "Cliente removido com sucesso."};
         }
 
 
