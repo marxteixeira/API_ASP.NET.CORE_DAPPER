acc52700ab8310c566edc35ef4e422602baadc82
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-09 09:27:01 -0300

    Criando estrutura básica de endpoints para o CRUD

---
+++ MxStore.Api/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MxStore.Domain.StoreContext.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MxStore.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        [Route("customers")]
+        public List<Customer> Get()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}")]
+        public List<Customer> GetById(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}/orders")]
+        public List<Order> GetOrders(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody] Customer customer)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public string Delete()
+        {
+            return null;
+        }
+
+
+
+    }
+}
--- MxStore.Api/Controllers/HomeController.cs
+++ MxStore.Api/Controllers/HomeController.cs
@@ -4,10 +4,32 @@ namespace MxStore.Api.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         [Route("")]
-        public object asdfasdfGet()
+        public object Get()
         {
             return new {version = "Version 0.0.1"};
+        }
+
+        [HttpPost]
+        [Route("")]
+        public object Post()
+        {
+            return new { version = "Version 0.0.1" };
+        }
+
+        [HttpPut]
+        [Route("")]
+        public object Put()
+        {
+            return new { version = "Version 0.0.1" };
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public object Delete()
+        {
+            return new { version = "Version 0.0.1" };
         }
     }
 }
